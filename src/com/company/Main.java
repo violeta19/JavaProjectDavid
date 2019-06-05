919e98875f86593f45b6f9b0134203f277fd2167
violeta19 <violeta.ciceu19@gmail.com>
2019-06-05 15:55:30 +0300

    am adaugat comentari la cod

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+        System.out.println("violeta");
+        char myChar='a';
+        byte myByte=123;
+        short myShort=12345;
+
+        float myFloat=12345;
+        String myString="avsfsgetdbdbu123456";
+
+        byte result=2+2;
+
+        System.out.println(2+2);
+
+    }
+}
